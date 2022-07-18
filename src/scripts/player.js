ffd382c0dac184762daf0616a0ebe5b057f087de
Ilia Lisov <ilya.lisov.22@gmail.com>
2022-07-18 18:00:28 +0400

    player release

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,118 +1,48 @@
-let player;
-const playerContainer = $(".player");
+const playerWrapper = document.querySelector('.player__wrapper');
+const player = document.querySelector('.player__elem');
+const playerControls = document.querySelector('.player__controls');
+const startBtn = document.querySelector('.player__start');
+const playback = document.querySelector('.player__playback');
+const playbackPoint = document.querySelector('.player__playback-button');
 
-let eventsInit = () => {
-    $(".player__start").click(e =>  {
-        e.preventDefault();
-
-        if (playerContainer.hasClass('paused')) {
-            player.pauseVideo()
-        } else {
-            player.playVideo()
-        }
-    });
-
-    $(".player__playback").click(e => {
-        const bar = $(e.currentTarget);
-        const clickedPosition = e.originalEvent.layerX;
-        const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
-        const newPlaybackPositionSec = (player.getDuration() / 100) * newButtonPositionPercent;
+const volume = document.querySelector('.player__volume-scale');
 
 
-        $(".player__playback-button").css({
-            left: `${newButtonPositionPercent}%`
-        });
-
-        player.seekTo(newPlaybackPositionSec);
-    });
-
-    $(".player__splash").click(e => {
-        player.playVideo();
-    })
-};
-
-const formatTime = timeSec => {
-    const roundTime = Math.round(timeSec);
-
-    const minutes = addZero(Math.floor(roundTime / 60));
-    const seconds = addZero(roundTime - minutes * 60);
-
-    function addZero(num) {
-        return num < 10 ? `0${num}` : num;
+let eventsInit = (e) => {
+    e.preventDefault();
+    if (player.paused) {
+        player.play();
+        startBtn.classList.add('active');
+    } else {
+        player.pause();
+        startBtn.classList.remove('active');
     }
-
-    return `${minutes} : ${seconds}`;
 }
 
-const onPlayerReady = () => {
-    let interval;
-    const durationSec = player.getDuration();
+startBtn.addEventListener('click', (e) => {
+    eventsInit(e);
+})
 
-    $('.player__duration-estimate').text(formatTime(durationSec));
+player.addEventListener('click', (e) => {
+    eventsInit(e);
+})
 
-    if (typeof interval != 'undefined') {
-        clearInterval(interval);
-    }
+volume.addEventListener('mousemove', (e) => {
+    player.volume = e.target.value
+})
 
-    interval = setInterval(() => {
-        const completeSec = player.getCurrentTime();
-        const completedPercent = (completeSec / durationSec) * 100;
+player.addEventListener('timeupdate', () => {
+    const completedPercent = (player.currentTime / player.duration) * 100;
+    playback.style.background = `linear-gradient(90deg, yellow ${completedPercent}%, #626262 0%)`;
+    playbackPoint.style.left = `${completedPercent}%`;
+})
 
-        $('.player__playback').css({
-            background:`linear-gradient(90deg, yellow ${completedPercent}%, #626262 0%)`
-        });
+playback.addEventListener('click', (e) => {
+    const progressTime = (e.offsetX / playback.offsetWidth) * player.duration;
+    player.currentTime = progressTime;
+})
 
-        $('.player__playback-button').css({
-            left: `${completedPercent}%`
-        });
-        $('.player__duration-completed').text(formatTime(completeSec));
-    }, 1000)
-}
-
-const onPlayerStateChange = event => {
-    /*
-    -1 – воспроизведение видео не началось
-    0 – воспроизведение видео завершено
-    1 – воспроизведение
-    2 – пауза
-    3 – буферизация
-    5 – видео находится в очереди
-    */
-   switch (event.data) {
-    case 1:
-        playerContainer.addClass("active");
-        playerContainer.addClass("paused");
-        break;
-
-    case 2:
-        playerContainer.removeClass("active");
-        playerContainer.removeClass("paused");
-        break;
-   }
-}
-
-const playerSectionContainer = $(".player-section__container");
-const playerHeight = playerSectionContainer.css("height");
-const playerWidth =  playerSectionContainer.css("width");
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('yt-player', {
-        height: playerHeight,
-        width: playerWidth,
-        videoId: 'KCLWvAcPcPk?autoplay=0&controls=0&disablekb=0&showinfo=0&modestbranding=1&rel=0',
-        playerVars: {
-            consrols: '0',
-            disablekb: '0',
-            schowinfo: '0',
-            rel: '0',
-            autoplay: '0',
-            modestbranding: '1'
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-
-eventsInit();
+document.querySelector(".player__volume-scale").oninput = function() {
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = `linear-gradient(90deg, yellow ${value}%, #626262 0%)`
+  };
